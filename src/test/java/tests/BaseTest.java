8d051210a3ecf8ec3454b615e723c18b9acc9b33
Borodich Timofei <borodich.timofei@gmail.com>
2025-09-04 16:06:01 +0300

    added secrets

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -23,8 +23,8 @@ public class BaseTest {
     ProductsPage productsPage;
     CartPage cartPage;
 
-    String user = System.getProperty("user");
-    String password = System.getProperty("password");
+    String user = System.getProperty("user", PropertyReader.getProperty("user"));
+    String password = System.getProperty("password", PropertyReader.getProperty("password"));
 
     @Parameters({"browser"})
     @BeforeMethod (description = "Настройка браузера", alwaysRun = true)
